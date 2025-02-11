5200b903a9f52d692b9c4b604e3b74efc31c98fe
p4irin <p4irin.github.io@gmail.com>
2025-02-11 12:42:34 +0100

    Add source_code_hash
    Terraform will update the lambda function if the source_code_hash changed

--- main.tf
+++ main.tf
@@ -7,8 +7,12 @@ data "archive_file" "lambda_motd" {
 resource "aws_lambda_function" "motd" {
     filename = "motd.zip"
     function_name = "motd"
+
     runtime = "nodejs18.x"
     handler = "index.handler"
+
+    source_code_hash = data.archive_file.lambda_motd.output_base64sha256
+    
     role = "arn:aws:iam::000000000000:role/lambda-role"
 }
 
